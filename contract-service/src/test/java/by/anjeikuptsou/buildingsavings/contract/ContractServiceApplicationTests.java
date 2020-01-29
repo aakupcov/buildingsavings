5da7b38013aa0f617af32459bb1c7da417874188
Andrey Kuptsov <andrey.kuptsov@specific-group.com>
2020-01-29 14:16:49 +0300

    Contract service for contract domain

---
+++ contract-service/src/test/java/by/anjeikuptsou/buildingsavings/contract/ContractServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.anjeikuptsou.buildingsavings.contract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
