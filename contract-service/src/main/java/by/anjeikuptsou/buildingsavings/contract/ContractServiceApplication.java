c0b5e0dafffd01b30ef65831d01e48d8d68831ff
Andrey Kuptsov <andrey.kuptsov@specific-group.com>
2020-01-29 18:14:17 +0300

    Contract Service swagger api is added

--- contract-service/src/main/java/by/anjeikuptsou/buildingsavings/contract/ContractServiceApplication.java
+++ contract-service/src/main/java/by/anjeikuptsou/buildingsavings/contract/ContractServiceApplication.java
@@ -2,9 +2,37 @@ package by.anjeikuptsou.buildingsavings.contract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.any;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ContractServiceApplication {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Contract").select()
+                .apis(RequestHandlerSelectors.basePackage("by.anjeikuptsou.buildingsavings.contract.controller"))
+                .paths(any()).build().apiInfo(new ApiInfo(
+                        "Contract Service",
+                        "A set of services to provide data access to contracts",
+                        "1.0.0",
+                        null,
+                        new Contact("Anjei Kuptsou", "https://www.linkedin.com/in/andrei-kuptsou-903aba124/", null),
+                        null,
+                        null,
+                        new ArrayList<VendorExtension>()));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ContractServiceApplication.class, args);
--- contract-service/src/main/java/by/anjeikuptsou/buildingsavings/contract/controller/ContractController.java
+++ contract-service/src/main/java/by/anjeikuptsou/buildingsavings/contract/controller/ContractController.java
@@ -2,6 +2,8 @@ package by.anjeikuptsou.buildingsavings.contract.controller;
 
 import by.anjeikuptsou.buildingsavings.contract.domain.Contract;
 import by.anjeikuptsou.buildingsavings.contract.service.ContractService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/contracts")
+@Api(value = "contracts", description = "Data service operations on contracts", tags = ("contracts"))
 public class ContractController {
     private final ContractService contractService;
 
@@ -21,6 +24,7 @@ public class ContractController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value = "Get all contracts", notes = "Get all contracts in the system", nickname = "getContracts")
     public List<Contract> findAll(@RequestParam(name = "contractNumber", required = false) String contractNumber) {
         return this.contractService.findAll(contractNumber);
     }
